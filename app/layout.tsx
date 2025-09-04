6504595aa89faf1acdc84796ddbf5ce34cde01c9
ROYPaul18 <114011395+ROYPaul18@users.noreply.github.com>
2025-09-04 17:32:58 +0200

    ajout de métadata en +

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,17 +24,82 @@ export const metadata: Metadata = {
   description:
     "Venez découvrir Link Salon à Soye-en-Septaine : un espace unique combinant tatouage, café et jeux de société. Une ambiance conviviale pour se détendre et s'exprimer artistiquement.",
   keywords: [
-    "Link Salon",
-    "salon tatouage Soye-en-Septaine",
-    "salon tatouage Bourges",
-    "salon tatouage Cher",
-    "tatouage asiatique",
-    "tatouage celtique",
-    "tatouage floral",
-    "tatouage animal",
-    "tatouage ornemental",
+    // Mots-clés principaux locaux
     "tatoueur Bourges",
-    "tatoueur région centre"
+    "salon tatouage Bourges",
+    "tatouage Bourges",
+    "meilleur tatoueur Bourges",
+    "tatoueur près de Bourges",
+
+    // Localisation précise
+    "tatoueur Soye-en-Septaine",
+    "salon tatouage Soye-en-Septaine",
+    "tatouage Cher 18",
+    "tatoueur département Cher",
+    "tatoueur région Centre",
+    "tatoueur Berry",
+
+    // Villes environnantes
+    "tatoueur Vierzon",
+    "tatoueur Saint-Doulchard",
+    "tatoueur Mehun-sur-Yèvre",
+    "tatoueur Dun-sur-Auron",
+    "tatoueur Saint-Germain-du-Puy",
+    "tatoueur Avord",
+    "tatoueur La Guerche-sur-l'Aubois",
+
+    // Styles de tatouage
+    "tatouage asiatique Bourges",
+    "tatouage japonais Bourges",
+    "tatouage celtique Bourges",
+    "tatouage floral Bourges",
+    "tatouage animal Bourges",
+    "tatouage ornemental Bourges",
+    "tatouage géométrique Bourges",
+    "tatouage réaliste Bourges",
+    "tatouage old school Bourges",
+    "tatouage tribal Bourges",
+    "tatouage minimaliste Bourges",
+    "tatouage noir et gris Bourges",
+    "tatouage couleur Bourges",
+
+    // Spécialités
+    "tatoueur femme Bourges",
+    "tatoueur homme Bourges",
+    "premier tatouage Bourges",
+    "retouche tatouage Bourges",
+    "cover up tatouage Bourges",
+    "tatouage sur cicatrice Bourges",
+
+    // Services connexes
+    "café tatouage Bourges",
+    "salon tatouage jeux société",
+    "tatoueur convivial Bourges",
+    "tatouage ambiance détendue",
+
+    // Marque et nom
+    "Link Salon",
+    "Gaël tatoueur",
+    "salon Link tatouage",
+
+    // Longue traîne locale
+    "où se faire tatouer à Bourges",
+    "bon tatoueur région Bourges",
+    "salon tatouage recommandé Bourges",
+    "tatoueur professionnel Cher",
+    "studio tatouage hygiène Bourges",
+    "prix tatouage Bourges",
+    "rendez-vous tatouage Bourges",
+    "consultation tatouage Bourges",
+
+    // Termes connexes
+    "tatouage artistique",
+    "art corporel Bourges",
+    "encre tatouage qualité",
+    "matériel tatouage stérilisé",
+    "hygiène tatouage",
+    "aftercare tatouage",
+    "cicatrisation tatouage",
   ],
   openGraph: {
     title: "Link Salon - Tatouage, Café et Jeux de Société",
@@ -68,7 +133,7 @@ export default function RootLayout({
         </Script>
         <meta
           name="description"
-          content="Venez découvrir Link Salon à Soye-en-Septaine : un espace unique combinant tatouage, café et jeux de société. Une ambiance conviviale pour se détendre et s'exprimer artistiquement."
+          content="Venez découvrir Link Salon à Soye-en-Septaine (5min de Bourges !): un espace unique combinant tatouage, café et jeux de société. Une ambiance conviviale pour se détendre et s'exprimer artistiquement."
         />
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="stylesheet" href={googleFontsURL} />
@@ -76,9 +141,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W47WH8KT"
-height="0" width="0" className="display:none;visibility:hidden"></iframe></noscript>
+
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W47WH8KT"
+          height="0" width="0" className="display:none;visibility:hidden"></iframe></noscript>
 
         {children}
       </body>
